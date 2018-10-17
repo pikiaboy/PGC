fd0273e8cc88f52315e4e3e4c228b1662845735c
John Dobrota <wickthebest@gmail.com>
2018-10-16 20:34:49 -0700

    connection functionality to mongoDB

--- be/index.js
+++ be/index.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 
 var express = require("express");
 var bodyParser = require("body-parser")
+var mongoose = require('mongoose')
+
 
 //All routes will be linked to this
 const routes = require('./routes');
@@ -14,6 +16,12 @@ app.use(bodyParser.json());
 
    
 app.use('/', routes);
+
+mongoose.connect('mongodb://testing1:testing1@ds135003.mlab.com:35003/pacificgcafe', (err) => {
+    if (!err)
+        console.log('connected successfully to monoDB')
+})
+
 
 
 var server = app.listen(8082, function () {
